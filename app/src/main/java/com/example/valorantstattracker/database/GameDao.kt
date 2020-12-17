a1d5fbb79fa12e1304ae17a53dd1a5b638f0defb
Matthew <m.opara25@gmail.com>
2020-12-17 14:33:49 -0800

    Change Constants to GameResult, Fix build bug

--- app/src/main/java/com/example/valorantstattracker/database/GameDao.kt
+++ app/src/main/java/com/example/valorantstattracker/database/GameDao.kt
@@ -5,7 +5,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
-import com.example.valorantstattracker.Constants
+import com.example.valorantstattracker.GameResult
 
 @Dao
 interface GameDao {
@@ -22,8 +22,8 @@ interface GameDao {
     @Query("SELECT * FROM game_table WHERE game_id = :id")
     suspend fun getGame(id: Long): Game?
 
-    @Query("SELECT * FROM game_table")
-    suspend fun getAllGames(): LiveData<List<Game>>
+    @Query("SELECT * FROM game_table ORDER BY game_id DESC")
+    fun getAllGames(): LiveData<List<Game>>
 
     @Query("SELECT * FROM game_table ORDER BY game_id DESC LIMIT :x")
     suspend fun getLastXGames(x: Int): List<Game>
@@ -31,12 +31,12 @@ interface GameDao {
     @Query("SELECT * FROM game_table WHERE agent_name = :agentName ORDER BY game_id DESC")
     suspend fun getGamesWithAgent(agentName: String): List<Game>
 
-    @Query("SELECT * FROM game_table WHERE result = ${Constants.WIN} ORDER BY game_id DESC")
+    @Query("SELECT * FROM game_table WHERE result = ${GameResult.WIN} ORDER BY game_id DESC")
     suspend fun getWonGames(): List<Game>
 
-    @Query("SELECT * FROM game_table WHERE result = ${Constants.LOSE} ORDER BY game_id DESC")
+    @Query("SELECT * FROM game_table WHERE result = ${GameResult.LOSE} ORDER BY game_id DESC")
     suspend fun getLostGames(): List<Game>
 
-    @Query("SELECT * FROM game_table WHERE result = ${Constants.DRAW} ORDER BY game_id DESC")
+    @Query("SELECT * FROM game_table WHERE result = ${GameResult.DRAW} ORDER BY game_id DESC")
     suspend fun getDrawGames(): List<Game>
 }
