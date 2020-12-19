397bc075c57d346e96c7a5a7b4977d21635a50b3
Matthew <m.opara25@gmail.com>
2020-12-18 20:40:39 -0800

    Create RecyclerView list in GamesFragment

--- app/src/main/java/com/example/valorantstattracker/games/GamesFragment.kt
+++ app/src/main/java/com/example/valorantstattracker/games/GamesFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.valorantstattracker.GameResult
+import com.example.valorantstattracker.database.Game
 import com.example.valorantstattracker.database.GameDatabase
 import com.example.valorantstattracker.databinding.FragmentGamesBinding
 
@@ -12,18 +15,49 @@ class GamesFragment : Fragment() {
 
     private lateinit var binding: FragmentGamesBinding
     private lateinit var gamesViewModel: GamesViewModel
+    private lateinit var gamesAdapter: GamesRecyclerAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         binding = FragmentGamesBinding.inflate(inflater)
         gamesViewModel = createGamesViewModel()
 
+          //For Testing
+//        for (i in 1..15) {
+//            gamesViewModel.createGame(makeGame(AgentName.OMEN))
+//        }
+
+        initRecyclerView()
+        displayGameHistory()
+
         return binding.root
+    }
+
+    // For Testing
+    private fun makeGame(agentName: String): Game {
+        return Game(entryTimeMilli = 10, result = GameResult.WIN,
+            agentName = agentName, combatScore = 100, kills = 20, deaths = 20,
+            assists = 20, econRating = 30, firstBloods = 2, plants = 2, defuses = 1)
     }
 
     private fun createGamesViewModel(): GamesViewModel {
         val application = requireNotNull(activity).application
         val dataSource = GameDatabase.getInstance(application).getGameDao()
         return GamesViewModel(dataSource, application)
+    }
+
+    private fun displayGameHistory() {
+        gamesViewModel.gameHistory.observe(viewLifecycleOwner, { gameHistory ->
+            gamesAdapter.submitList(gameHistory)
+            binding.recyclerView.adapter = gamesAdapter
+        })
+    }
+
+    private fun initRecyclerView() {
+        gamesAdapter = GamesRecyclerAdapter(resources)
+        binding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@GamesFragment.context)
+            adapter = gamesAdapter
+        }
     }
 }
