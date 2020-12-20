6e876042617630f6ed177ad24da4d55804cc2cee
Matthew <m.opara25@gmail.com>
2020-12-20 15:39:43 -0800

    Add warning for invalid data entry when clicking confirm button

--- app/src/main/java/com/example/valorantstattracker/games/GameEntryFragment.kt
+++ app/src/main/java/com/example/valorantstattracker/games/GameEntryFragment.kt
@@ -1,15 +1,19 @@
 package com.example.valorantstattracker.games
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.core.widget.doAfterTextChanged
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import com.example.valorantstattracker.Agent
+import com.example.valorantstattracker.ExposedDropDownMenu
 import com.example.valorantstattracker.R
 import com.example.valorantstattracker.databinding.FragmentGameEntryBinding
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 private const val AGENT_KEY = "AGENT"
@@ -18,27 +22,58 @@ private const val RESULT_KEY = "RESULT"
 class GameEntryFragment : Fragment() {
 
     private lateinit var binding: FragmentGameEntryBinding
+    private lateinit var dropdownMenus: List<ExposedDropDownMenu>
+    private lateinit var textInputs: List<TextInputEditText>
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
 
         binding = FragmentGameEntryBinding.inflate(inflater)
+        dropdownMenus = listOf(binding.agentMenu, binding.gameResultMenu)
+        textInputs = listOf(binding.combatScoreInput,
+            binding.killsInput,
+            binding.deathsInput,
+            binding.assistsInput,
+            binding.econRatingInput,
+            binding.firstBloodsInput,
+            binding.plantsInput,
+            binding.defusesInput)
         prepareForDataRetrieval(savedInstanceState)
 
+        binding.confirmButton.setOnClickListener {
+            var hasInvalidInputs = false
+            for (menu in dropdownMenus) {
+                if (menu.text.toString().isEmpty()) {
+                    hasInvalidInputs = true
+                    menu.error = getString(R.string.error_input_required)
+                }
+            }
+            for (editText in textInputs) {
+                if (editText.error != null) {
+                    hasInvalidInputs = true
+                } else if (editText.text.toString().isEmpty()) {
+                    hasInvalidInputs = true
+                    editText.error = getString(R.string.error_input_required)
+                }
+            }
+
+            if (hasInvalidInputs) {
+                val snackbar = Snackbar.make(binding.coordinatorLayout, getString(R.string.invalid_data), Snackbar.LENGTH_SHORT)
+                snackbar.show()
+            } else {
+                // TODO: make a new game, insert it into the DB, and navigate to games fragment
+                Log.d("GameEntryFragment", "Save and Navigate")
+            }
+        }
         return binding.root
     }
 
     private fun prepareForDataRetrieval(savedInstanceState: Bundle?) {
         setUpAgentMenu(savedInstanceState)
         setUpResultMenu(savedInstanceState)
-        setUpTextInput(binding.combatScoreInput)
-        setUpTextInput(binding.killsInput)
-        setUpTextInput(binding.deathsInput)
-        setUpTextInput(binding.assistsInput)
-        setUpTextInput(binding.econRatingInput)
-        setUpTextInput(binding.firstBloodsInput)
-        setUpTextInput(binding.defusesInput)
-        setUpTextInput(binding.plantsInput)
+        for (editText in textInputs) {
+            setUpTextInput(editText)
+        }
     }
 
     private fun setUpAgentMenu(savedInstanceState: Bundle?) {
@@ -46,6 +81,9 @@ class GameEntryFragment : Fragment() {
         val agentAdapter =
             ArrayAdapter(requireContext(), R.layout.text_list_item, Agent.getAgentList())
         binding.agentMenu.setAdapter(agentAdapter)
+        binding.agentMenu.doAfterTextChanged {
+            binding.agentMenu.error = null
+        }
     }
 
     private fun setUpResultMenu(savedInstanceState: Bundle?) {
@@ -57,6 +95,9 @@ class GameEntryFragment : Fragment() {
         )
         val resultAdapter = ArrayAdapter(requireContext(), R.layout.text_list_item, resultList)
         binding.gameResultMenu.setAdapter(resultAdapter)
+        binding.gameResultMenu.doAfterTextChanged {
+            binding.gameResultMenu.error = null
+        }
     }
 
     private fun setUpTextInput(editText: TextInputEditText) {
@@ -67,10 +108,18 @@ class GameEntryFragment : Fragment() {
 
     private fun makeErrorMessage(inputText: CharSequence?): String? {
         val isInvalidInput = inputText?.any { !it.isDigit() }
-        return if (isInvalidInput == true) {
-            getString(R.string.error_int)
-        } else {
-            null
+        val isNoInput = inputText?.isEmpty()
+
+        return when {
+            isInvalidInput == true -> {
+                getString(R.string.error_number_required)
+            }
+            isNoInput == true -> {
+                getString(R.string.error_input_required)
+            }
+            else -> {
+                null
+            }
         }
     }
 
