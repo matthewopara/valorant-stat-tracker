2b5f8b148fd197d09c644e501600629235ac2788
Matthew <m.opara25@gmail.com>
2020-12-20 18:27:39 -0800

    Add method to convert a game result from string form to its corresponding integer

--- app/src/main/java/com/example/valorantstattracker/GameResult.kt
+++ app/src/main/java/com/example/valorantstattracker/GameResult.kt
@@ -1,7 +1,18 @@
 package com.example.valorantstattracker
 
+import android.content.res.Resources
+
 object GameResult {
     const val WIN: Int = 0
     const val LOSE: Int = 1
     const val  DRAW: Int = 2
+
+    fun convertResultTextToInt(result: String, resources: Resources): Int? {
+        return when (result) {
+            resources.getString(R.string.win) -> WIN
+            resources.getString(R.string.lose) -> LOSE
+            resources.getString(R.string.draw) -> DRAW
+            else -> null
+        }
+    }
 }
