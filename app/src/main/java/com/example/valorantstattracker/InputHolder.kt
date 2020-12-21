66ca6a0b8c286c067c9f74f465d380917598822d
Matthew <m.opara25@gmail.com>
2020-12-21 14:04:39 -0800

    Add InputHolder

---
+++ app/src/main/java/com/example/valorantstattracker/InputHolder.kt
@@ -0,0 +1,14 @@
+package com.example.valorantstattracker
+
+data class InputHolder(
+    var agentNameInput: String = "",
+    var gameResultInput: String = "",
+    var combatScoreInput: String = "",
+    var killsInput: String = "",
+    var deathsInput: String = "",
+    var assistsInput: String = "",
+    var econRatingInput: String = "",
+    var firstBloodsInput: String = "",
+    var plantsInput: String = "",
+    var defusesInput: String = ""
+)
