71e5e214eb83bbecc1ba227bee93952e7a8cc9bb
Matthew <m.opara25@gmail.com>
2020-12-20 17:17:10 -0800

    Added kotlin-parcelize plugin & parcelized Game data class

--- app/src/main/java/com/example/valorantstattracker/database/Game.kt
+++ app/src/main/java/com/example/valorantstattracker/database/Game.kt
@@ -1,9 +1,12 @@
 package com.example.valorantstattracker.database
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity(tableName = "game_table")
 data class Game(
     @PrimaryKey(autoGenerate = true)
@@ -33,4 +36,4 @@ data class Game(
 
     val plants: Int,
     val defuses: Int,
-)
+) : Parcelable
