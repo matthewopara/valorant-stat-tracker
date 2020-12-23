a7cc49efbd4f7fb4b8c0818b0c95b61249ee8f8e
Matthew <m.opara25@gmail.com>
2020-12-23 01:07:15 -0800

    Delete ItemClickListener.kt & ItemLongClickListener.kt

--- app/src/main/java/com/example/valorantstattracker/ItemClickListener.kt
+++
@@ -1,7 +0,0 @@
-package com.example.valorantstattracker
-
-import android.view.View
-
-interface ItemClickListener {
-    fun onItemClick(view: View, position: Int)
-}
--- app/src/main/java/com/example/valorantstattracker/ItemLongClickListener.kt
+++
@@ -1,7 +0,0 @@
-package com.example.valorantstattracker
-
-import android.view.View
-
-interface ItemLongClickListener {
-    fun onItemLongClick(view: View, position: Int)
-}
