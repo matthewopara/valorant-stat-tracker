07ca1d0f82661380abeccc5e5158b6176008eff5
Matthew <m.opara25@gmail.com>
2020-12-29 22:05:39 -0800

    Add agent images on chart entry press

--- app/src/main/java/com/example/valorantstattracker/insights/GameResultChart.kt
+++ app/src/main/java/com/example/valorantstattracker/insights/GameResultChart.kt
@@ -6,10 +6,13 @@ import com.example.valorantstattracker.R
 import com.example.valorantstattracker.database.Game
 import com.example.valorantstattracker.objects.GameResult
 import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 
 class GameResultChart(val chart: PieChart, private val resources: Resources) {
 
@@ -29,6 +32,25 @@ class GameResultChart(val chart: PieChart, private val resources: Resources) {
         chart.setUsePercentValues(true)
         chart.legend.setCustom(emptyArray())
         chart.description.isEnabled = false
+    }
+
+    fun setOnValuePressedListener(listener: (List<GameResultTracker>) -> Unit) {
+        chart.setOnChartValueSelectedListener(object : OnChartValueSelectedListener {
+            override fun onValueSelected(e: Entry?, h: Highlight?) {
+                e?.data?.let { data ->
+                    when (data) {
+                        GameResult.WIN -> { listener(allTrackers.toList().sortedByDescending { it.winCount }) }
+                        GameResult.LOSE -> { listener(allTrackers.toList().sortedByDescending { it.loseCount }) }
+                        GameResult.DRAW -> { listener(allTrackers.toList().sortedByDescending { it.drawCount }) }
+                        else -> { listener(emptyList()) }
+                    }
+                }
+            }
+
+            override fun onNothingSelected() {
+                listener(emptyList())
+            }
+        })
     }
 
     fun setGames(games: List<Game>) {
@@ -59,9 +81,9 @@ class GameResultChart(val chart: PieChart, private val resources: Resources) {
 
     private fun updateChart() {
         val entries = arrayListOf<PieEntry>()
-        val winEntry = PieEntry(numOfWins, resources.getString(R.string.win))
-        val loseEntry = PieEntry(numOfLoses, resources.getString(R.string.lose))
-        val drawEntry = PieEntry(numOfDraws, resources.getString(R.string.draw))
+        val winEntry = PieEntry(numOfWins, resources.getString(R.string.win), GameResult.WIN)
+        val loseEntry = PieEntry(numOfLoses, resources.getString(R.string.lose), GameResult.LOSE)
+        val drawEntry = PieEntry(numOfDraws, resources.getString(R.string.draw), GameResult.DRAW)
         entries.addAll(listOf(winEntry, loseEntry, drawEntry))
 
         val dataSet = PieDataSet(entries, "")
--- app/src/main/java/com/example/valorantstattracker/insights/InsightsFragment.kt
+++ app/src/main/java/com/example/valorantstattracker/insights/InsightsFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import com.example.valorantstattracker.MainActivity
 import com.example.valorantstattracker.database.GameDatabase
 import com.example.valorantstattracker.databinding.FragmentInsightsBinding
+import com.example.valorantstattracker.objects.Agent
 import com.example.valorantstattracker.objects.BasicUIUtil
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -29,6 +30,27 @@ class InsightsFragment : Fragment() {
         CoroutineScope(Dispatchers.IO).launch {
             val lastGames = GameDatabase.getInstance(requireContext()).getGameDao().getLastXGames(10)
             gameResultChart.setGames(lastGames)
+        }
+
+        gameResultChart.setOnValuePressedListener {
+            val agentImageViews = listOf(binding.firstAgentImage, binding.secondAgentImage, binding.thirdAgentImage)
+            var count = it.size
+            if (count > agentImageViews.size) {
+                count = agentImageViews.size
+            }
+
+            for (imageView in agentImageViews) {
+                imageView.visibility = View.INVISIBLE
+            }
+
+            for (i in 0 until count) {
+                agentImageViews[i].visibility = View.VISIBLE
+                val imgRes = Agent.getImageResource(it[i].agentName)
+                imgRes?.let { res ->
+                    agentImageViews[i].setImageResource(res)
+                    agentImageViews[i].contentDescription = it[i].agentName
+                }
+            }
         }
 
         return binding.root
